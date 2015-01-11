c557edb45e4f06c75826913b81573fdc381189ca
Valentine Gogichashvili <valgog@gmail.com>
2015-01-11 21:49:58 +0100

    added tests and travis-ci configuration

--- app/controllers/RegistryController.scala
+++ app/controllers/RegistryController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import play.api.libs.iteratee.Enumerator
 import play.api.libs.json._
 import play.api.mvc._
 import scala.collection.JavaConverters._
@@ -11,61 +10,73 @@ import scala.util.{Failure, Success, Try}
 import java.nio.file.{Paths, Files, StandardOpenOption}
 import java.nio.charset.{StandardCharsets}
 
+object DockerHeaders {
+  val REGISTRY_VERSION = "X-Docker-Registry-Version"
+  val TOKEN            = "X-Docker-Token"
+  val ENDPOINTS        = "X-Docker-Endpoints"
+}
+import DockerHeaders._
+
 object RegistryController extends Controller {
 
   val dataPath = Paths.get("data")
 
+  private def asJson(r: Result): Result = r.as("application/json")
+
   def _ping() = Action {
-    Ok(Json.toJson(true)).withHeaders("X-Docker-Registry-Version" -> "0.6.3")
+    asJson(Ok(Json.toJson(true)).withHeaders(REGISTRY_VERSION -> "0.6.3"))
   }
 
   def images(repo: String) = Action {
-    Ok(Json.toJson("a"))
+    NotImplemented
   }
 
   def putImages(repo: String) = Action {
-    // TODO
-    Status(204)("")
+    NotImplemented
   }
 
-  def putRepo(repo: String) = Action {implicit request =>
+  def putRepo(repo: String) = Action {request =>
     val host: String = request.headers.get("Host").getOrElse("")
-    Ok(Json.toJson("PUTPUT")).withHeaders("X-Docker-Token" -> "mytok").withHeaders("X-Docker-Endpoints" -> host)
+    asJson(
+      Ok(Json.toJson("PUTPUT"))
+      .withHeaders(TOKEN -> "mytok")
+      .withHeaders(ENDPOINTS -> host)
+    )
   }
 
   def getImageJson(image: String) = Action {
-    val imagePath = dataPath.resolve(image + ".json")
-    val layerPath = dataPath.resolve(image + ".layer")
+    val imagePath = dataPath.resolve(s"${image}.json")
+    val layerPath = dataPath.resolve(s"${image}.layer")
     if (Files.exists(imagePath) && Files.exists(layerPath)) {
-      val contents = Files.readAllLines(dataPath.resolve(image + ".json"), StandardCharsets.UTF_8).asScala.mkString
-      Ok(Json.parse(contents))
+      val contents = Files.readAllLines(imagePath, StandardCharsets.UTF_8).asScala.mkString
+      asJson(Ok(Json.parse(contents)))
     } else {
-      NotFound("Image JSON not found")
+      asJson(NotFound(Json.toJson(s"Image JSON (${image}.json) not found")))
     }
   }
 
   def putImageJson(image: String) = Action(BodyParsers.parse.json) { request =>
     Files.createDirectories(dataPath)
-    val contents = Json.stringify(request.body)
+    val contents: String = Json.stringify(request.body)
 
-    Files.write(dataPath.resolve(image + ".json"), contents.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE)
-    Ok(Json.toJson("OK"))
+    Files.write(dataPath.resolve(s"${image}.json"), contents.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE)
+    asJson(Ok(Json.toJson("OK")))
   }
 
   def putImageLayer(image: String) = Action(BodyParsers.parse.temporaryFile) { request =>
-    val layerPath = dataPath.resolve(image + ".layer")
+    val layerPath = dataPath.resolve(s"${image}.layer")
     val bodyData = request.body.moveTo(layerPath.toFile)
-    Ok(Json.toJson("OK"))
+    asJson(Ok(Json.toJson("OK")))
   }
 
   def putImageChecksum(image: String) = Action {
+    NotImplemented
     // TODO: do something
-    Ok(Json.toJson("OK"))
   }
 
   def putTag(repo: String, tag:String) = Action {
+    NotImplemented
     // TODO: save tag
-    Ok(Json.toJson("OK"))
   }
 }
 
---
+++ test/RegistryControllerTest.scala
@@ -0,0 +1,28 @@
+import org.specs2.mutable._
+import play.api.libs.json.JsBoolean
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, WithApplication}
+import controllers.DockerHeaders._
+
+class RegistryControllerTest extends Specification {
+  "RegistryController" should {
+    "respond to _ping" in new WithApplication {
+      val res = route(FakeRequest(GET, "/v1/_ping")).get
+
+      val reportedDockerRegistryVersion = header(REGISTRY_VERSION, res).getOrElse("UNKNOWN")
+
+      reportedDockerRegistryVersion must_== "0.6.3"
+
+      status(res) must_== 200
+      contentAsJson(res) match {
+        case JsBoolean(value) => value must_== true
+        case other => sys.error(s"Unexpected result type: $other")
+      }
+    }
+    "fail on getting JSON image for non-exiting image" in new WithApplication {
+      val res = route(FakeRequest(GET, "/v1/images/NONEXISTING/json")).get
+      status(res) must_== 404
+    }
+  }
+
+}
